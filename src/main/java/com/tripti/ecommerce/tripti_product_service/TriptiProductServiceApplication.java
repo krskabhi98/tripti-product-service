9c2f650face945e7cc1500c3e62c15bdda439fa1
abhishek <kr.skabhi@gmail.com>
2024-06-23 04:06:20 +0530

    first commit

---
+++ src/main/java/com/tripti/ecommerce/tripti_product_service/TriptiProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tripti.ecommerce.tripti_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriptiProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriptiProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripti/ecommerce/tripti_product_service/TriptiProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripti.ecommerce.tripti_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriptiProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
